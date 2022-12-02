93bd3781d7ce3d886627fc6fcb069b29ac2d1195
TonyMike <tee.jhay1@gmail.com>
2022-12-02 09:20:27 +0100

    travel blog backend

---
+++ schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'category',
+  type: 'document',
+  title: 'Category',
+  fields: [
+    {
+      name: 'name',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    }
+  ]
+}
